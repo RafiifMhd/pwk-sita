ff5f00ae666eb31a13c99946fccf54abe45674e2
Muhammad Rafiif <muhammad.120140148@student.itera.ac.id>
2025-05-20 13:22:15 +0700

    fix 1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: 'https://pwk-sita-production.up.railway.app/',
     plugins: [
         laravel({
             input: [
